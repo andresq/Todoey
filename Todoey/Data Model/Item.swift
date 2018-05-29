95402379ca435cc08cef88b385a54259d9138362
Andres Quintero <hello@andresq.com>
2018-05-28 20:59:47 -0400

    structured to MVC design

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Andres Quintero on 5/28/18.
+//  Copyright © 2018 Andres Quintero. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
